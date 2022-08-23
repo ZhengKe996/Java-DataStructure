1369e8e9f3eab48004ff8770419708887480e1e4
ZhengKe <zhengke996@163.com>
2022-08-23 13:16:08 +0800

    feat: 自底向上的归并排序法

--- src/MergeSort/MergeSort.java
+++ src/MergeSort/MergeSort.java
@@ -12,6 +12,12 @@ public class MergeSort<T>{
     private MergeSort(){
     }
 
+    /**
+     * 自顶向下的归并排序
+     *
+     * @param arr
+     * @param <T>
+     */
     public static <T extends Comparable<T>> void sort(T[] arr){
         T[] temp = Arrays.copyOf(arr,arr.length);
         sort(arr,0,arr.length - 1,temp);
@@ -32,6 +38,26 @@ public class MergeSort<T>{
         sort(arr,l,mid,temp);
         sort(arr,mid + 1,r,temp);
         if(arr[mid].compareTo(arr[mid + 1]) > 0) merge(arr,l,mid,r,temp);
+    }
+
+    /**
+     * 自底向上的归并排序
+     *
+     * @param arr
+     * @param <T>
+     */
+    public static <T extends Comparable<T>> void sortBU(T[] arr){
+        T[] temp = Arrays.copyOf(arr,arr.length);
+        int n = arr.length;
+
+        // 遍历合并的区间长度
+        for(int sz = 1;sz < n;sz += sz){
+            // 遍历合并的两个区间的起始位置 i  合并 [i, i + sz - 1] 和 [i + sz, Math.min(i + sz + sz - 1, n - 1)]
+            for(int i = 0;i + sz < n;i += sz + sz){
+                if(arr[i + sz - 1].compareTo(arr[i + sz]) > 0)
+                    merge(arr,i,i + sz - 1,Math.min(i + sz + sz - 1,n - 1),temp);
+            }
+        }
     }
 
     /**
@@ -66,8 +92,10 @@ public class MergeSort<T>{
     }
 
     public static void main(String[] args){
-        int n = 100000;
+        int n = 5000000;
         Integer[] array = ArrayGenerator.generateRandomArray(n,n);
+        Integer[] array2 = Arrays.copyOf(array,array.length);
         SortingHelper.sortTest("MergeSort",array);
+        SortingHelper.sortTest("MergeSortBU",array2);
     }
 }
