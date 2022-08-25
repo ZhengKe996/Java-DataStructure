6b689b4823451a62b0fea3656cc1769da3f6976c
ZhengKe <zhengke996@163.com>
2022-08-25 13:56:54 +0800

    feat: 三路快速排序法

--- src/QuickSort/QuickSort.java
+++ src/QuickSort/QuickSort.java
@@ -16,10 +16,29 @@ public class QuickSort{
 
     private static <T extends Comparable<T>> void sort(T[] arr,int l,int r,Random rnd){
         if(l >= r) return;
-        // 标定点索引
-        int p = partition(arr,l,r,rnd);
-        sort(arr,l,p - 1,rnd);
-        sort(arr,p + 1,r,rnd);
+
+        // 生成 [l,r] 之间的随机索引
+        int p = l + rnd.nextInt(r - l + 1);
+        swap(arr,l,p);
+
+        // arr[l+1,lt]<v, arr[lt+1,i-1] == v,arr[gt,r] > v
+        int lt = l, i = l + 1, gt = r + 1;
+        while(i < gt){
+            if(arr[i].compareTo(arr[l]) < 0){
+                lt++;
+                swap(arr,i,lt);
+                i++;
+            }else if(arr[i].compareTo(arr[l]) > 0){
+                gt--;
+                swap(arr,i,gt);
+            }else{
+                i++;
+            }
+        }
+        // arr[l,lt-1]<v, arr[lt,gt-1] == v,arr[gt,r] > v
+        swap(arr,l,lt);
+        sort(arr,l,lt - 1,rnd);
+        sort(arr,gt,r,rnd);
     }
 
     private static <T extends Comparable<T>> int partition(T[] arr,int l,int r,Random rnd){
