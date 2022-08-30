c693e64de9e2d5c29f27433f860807ab1aa996cb
ZhengKe <zhengke996@163.com>
2022-08-30 12:02:08 +0800

    feat: 二分变种 ceil

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -33,4 +33,12 @@ public class BinarySearch{
         }
         return l;
     }
+
+    // > target，返回最小值索引
+    // == target，返回最大索引
+    public static <E extends Comparable<E>> int ceil(E[] data,E target){
+        int u = upper(data,target);
+        if(u - 1 >= 0 && data[u - 1].compareTo(target) == 0) return u - 1;
+        return u;
+    }
 }
