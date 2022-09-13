0e536f9e847d19131497bbd5714fc998790ddd9a
ZhengKe <zhengke996@163.com>
2022-09-13 19:45:35 +0800

    feat: 二分搜索树 中序遍历

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -80,6 +80,20 @@ public class BST<E extends Comparable<E>>{
         preOrder(node.right);
     }
 
+    // 二分搜索树 中序遍历
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(Node node){
+        if(node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
