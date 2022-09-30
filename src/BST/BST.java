7613e189fff0b3a61a912d7b6b852793191af242
ZhengKe <zhengke996@163.com>
2022-09-30 19:48:54 +0800

    feat: 二分搜索树 删除二分搜索树的最大值/最小值

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -1,8 +1,6 @@
 package BST;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class BST<E extends Comparable<E>>{
     private class Node{
@@ -29,7 +27,7 @@ public class BST<E extends Comparable<E>>{
         return size;
     }
 
-    public boolean isEmpty(int size){
+    public boolean isEmpty(){
         return size == 0;
     }
 
@@ -140,6 +138,66 @@ public class BST<E extends Comparable<E>>{
             if(cur.right != null)
                 q.add(cur.right);
         }
+    }
+
+    // 寻找二分搜索树的最小元素（递归）
+    public E minimum(){
+        if(size == 0) throw new IllegalArgumentException("BST is empty");
+
+        return minimum(root).e;
+    }
+
+    private Node minimum(Node node){
+        if(node.left == null) return node;
+        return minimum(node.left);
+    }
+
+    // 寻找二分搜索树的最大元素（递归）
+    public E maximum(){
+        if(size == 0) throw new IllegalArgumentException("BST is empty");
+
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node){
+        if(node.right == null) return node;
+        return minimum(root.left);
+    }
+
+    // 删除二分搜索树的最小值节点
+    public E removeMin(){
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    private Node removeMin(Node node){
+        if(node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    // 删除二分搜索树的最大值节点
+    public E removeMax(){
+        E ret = maximum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    private Node removeMax(Node node){
+        if(node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
     }
 
 
