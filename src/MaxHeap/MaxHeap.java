4d95cc6611aabca0464a747a0db2ad1b41429087
ZhengKe <zhengke996@163.com>
2022-10-03 16:47:22 +0800

    feat: 堆 向堆添加元素 SiftUp

--- src/MaxHeap/MaxHeap.java
+++ src/MaxHeap/MaxHeap.java
@@ -51,4 +51,33 @@ public class MaxHeap<E extends Comparable<E>> {
         return (index + 1) * 2;
     }
 
+    /**
+     * 向堆中添加元素
+     *
+     * @param e
+     */
+    public void add(E e) {
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+
+    /**
+     * siftUp：
+     * 堆中某个节点的值总是不大于其父节点的值, 超过即需要交换位置
+     *
+     * @param k
+     */
+    private void siftUp(int k) {
+        /**
+         * 交换条件：
+         * 1. 下标大于0
+         * 2. 自身值大于父节点的值
+         */
+        while (k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0) {
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+
+
 }
