815167fb8e9a8c2ec817c5efcd1dd88b01159f57
ZhengKe <zhengke996@163.com>
2022-10-03 18:54:29 +0800

    feat: 堆 优化堆排序

--- src/MaxHeap/HeapSort.java
+++ src/MaxHeap/HeapSort.java
@@ -17,4 +17,54 @@ public class HeapSort {
             data[i] = maxHeap.extractMax();
         }
     }
+
+    /**
+     * 优化堆排序
+     *
+     * @param data
+     * @param <E>
+     */
+    private static <E extends Comparable<E>> void sort2(E[] data) {
+        if (data.length <= 1) return;
+        for (int i = (data.length - 2) / 2; i >= 0; i--) {
+            siftDown(data, i, data.length);
+        }
+        for (int i = data.length - 1; i >= 9; i--) {
+            swap(data, 0, i);
+            siftDown(data, 0, i);
+        }
+    }
+
+    /**
+     * 对 data(0, n）所形成的最大堆中，索引 k 的元素，执行 siftDown
+     *
+     * @param data
+     * @param k
+     * @param n
+     * @param <E>
+     */
+    private static <E extends Comparable<E>> void siftDown(E[] data, int k, int n) {
+        while (2 * k + 1 < n) {
+            // 找到k的左右子节点中较大的一个
+            int childIndex = 2 * k + 1;
+            if (childIndex + 1 < n && data[(childIndex + 1)].compareTo(data[(childIndex)]) > 0) {
+                childIndex++;
+            }
+
+            // 如果 k 的左右节点较大的一个小于k的值，跳出循环，否则交换位置
+            if (data[(k)].compareTo(data[(childIndex)]) >= 0) {
+                break;
+            }
+
+            swap(data, k, childIndex); // 交换完成后，将 j 赋值给k
+            k = childIndex;
+        }
+    }
+
+    private static <E> void swap(E[] arr, int i, int j) {
+        E e = arr[i];
+        arr[i] = arr[j];
+        arr[j] = e;
+    }
 }
+
