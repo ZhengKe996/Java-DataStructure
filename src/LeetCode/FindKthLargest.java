75c8981b26d5832477be033f4e9891ad2e234241
ZhengKe <zhengke996@163.com>
2022-10-04 13:18:16 +0800

    feat: LeetCode 215 优先队列

--- src/LeetCode/FindKthLargest.java
+++ src/LeetCode/FindKthLargest.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.PriorityQueue;
 import java.util.Random;
 
 /**
@@ -13,41 +14,55 @@ import java.util.Random;
  * 链接：https://leetcode.cn/problems/kth-largest-element-in-an-array
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class FindKthLargest{
-    public int findKthLargest(int[] nums,int k){
+public class FindKthLargest {
+    public int findKthLargest(int[] nums, int k) {
 
         Random rnd = new Random();
-        return selectK(nums,0,nums.length - 1,nums.length - k,rnd);
+        return selectK(nums, 0, nums.length - 1, nums.length - k, rnd);
     }
 
-    private int selectK(int[] arr,int l,int r,int k,Random rnd){
-        int p = partition(arr,l,r,rnd);
-        if(k == p) return arr[p];
-        if(k < p) return selectK(arr,l,p - 1,k,rnd);
-        return selectK(arr,p + 1,r,k,rnd);
+    private int selectK(int[] arr, int l, int r, int k, Random rnd) {
+        int p = partition(arr, l, r, rnd);
+        if (k == p) return arr[p];
+        if (k < p) return selectK(arr, l, p - 1, k, rnd);
+        return selectK(arr, p + 1, r, k, rnd);
     }
 
-    private int partition(int[] arr,int l,int r,Random rnd){
+    private int partition(int[] arr, int l, int r, Random rnd) {
         int p = l + rnd.nextInt(r - l + 1);
-        swap(arr,l,p);
+        swap(arr, l, p);
         int i = l + 1, j = r;
-        while(true){
-            while(i <= j && arr[i] < arr[l]) i++;
-            while(j >= i && arr[j] > arr[l]) j--;
+        while (true) {
+            while (i <= j && arr[i] < arr[l]) i++;
+            while (j >= i && arr[j] > arr[l]) j--;
 
-            if(i >= j) break;
-            swap(arr,i,j);
+            if (i >= j) break;
+            swap(arr, i, j);
             i++;
             j--;
         }
-        swap(arr,l,j);
+        swap(arr, l, j);
         return j;
     }
 
-    private void swap(int[] nums,int i,int j){
+    private void swap(int[] nums, int i, int j) {
         int t = nums[i];
         nums[i] = nums[j];
         nums[j] = t;
     }
 
+    public int findKthLargest2(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            if (!pq.isEmpty() && nums[i] > pq.peek()) {
+                pq.remove();
+                pq.add(nums[i]);
+            }
+        }
+        return pq.peek();
+    }
 }
