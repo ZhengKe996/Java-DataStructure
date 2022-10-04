43ee98ef023c410117187a8d6e9ab46fefe8797b
ZhengKe <zhengke996@163.com>
2022-10-04 14:05:33 +0800

    feat: 排序 冒泡排序优化

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -9,11 +9,14 @@ public class BubbleSort {
 
     public static <E extends Comparable<E>> void sort(E[] data) {
         for (int i = 0; i + 1 < data.length; i++) {
+            boolean isSwapped = false;
             for (int j = 0; j < data.length - i - 1; j++) {
                 if (data[j].compareTo(data[j + 1]) > 0) {
                     swap(data, j, j + 1);
+                    isSwapped = true;
                 }
             }
+            if (!isSwapped) break;
         }
     }
 
