c732773c28868a4fc4774a00d8a28ac4c2649d68
ZhengKe <zhengke996@163.com>
2022-10-04 14:01:00 +0800

    feat: 排序 冒泡排序

---
+++ src/BubbleSort/BubbleSort.java
@@ -0,0 +1,25 @@
+package BubbleSort;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+    private BubbleSort() {
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] data) {
+        for (int i = 0; i + 1 < data.length; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j].compareTo(data[j + 1]) > 0) {
+                    swap(data, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static <E> void swap(E[] arr, int i, int j) {
+        E e = arr[i];
+        arr[i] = arr[j];
+        arr[j] = e;
+    }
+}
