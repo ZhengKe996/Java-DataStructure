163af09356b293e83fd820f908697e3689231901
ZhengKe <zhengke996@163.com>
2022-10-05 19:28:34 +0800

    feat: LeetCode 解题

---
+++ src/LeetCode/NumArray/NumArray2.java
@@ -0,0 +1,18 @@
+package LeetCode.NumArray;
+
+public class NumArray2 {
+
+    private int[] sum;
+
+    public NumArray2(int[] nums) {
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+}
---
+++ src/LeetCode/NumArray/NumArray4.java
@@ -0,0 +1,31 @@
+package LeetCode.NumArray;
+
+public class NumArray4 {
+
+    private int[] sum;
+    private int[] data;
+
+    public NumArray4(int[] nums) {
+        data = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            data[i] = nums[i];
+        }
+
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public void update(int index, int val) {
+        data[index] = val;
+        for (int i = index + 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + data[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+}
